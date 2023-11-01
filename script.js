1fbf5d2c9f3c0d5a740826f0f75ab6d8f059c1e3
Marlon Willian Silva Barros <mwsb2004@gmail.com>
2023-10-31 21:36:03 -0300

    js concluído

--- script.js
+++ script.js
@@ -1,51 +1,93 @@
 // características das plantas
 
-let euphorbia = {nome: "Euphorbia eritrea", sol: "high", water: "rarely", preço: "25", toxicity: "false"}
+let euphorbia = {sol: "high", water: "rarely", preço: "25", toxicity: "false"}
 
-let succculent = {nome: "Succculent Bowl", sol: "high", water: "rarely", preço: "30", toxicity: "false"}
+let succculent = {sol: "high", water: "rarely", preço: "30", toxicity: "false"}
 
-let bunny = {nome: "Bunny ears cacti", sol: "high", water: "rarely", preço: "20", toxicity: "false"}
+let bunny = {sol: "high", water: "rarely", preço: "20", toxicity: "false"}
 
-let ficus = {nome: "Ficus lyrata", sol: "high", water: "regulary", preço: "30", toxicity: "false"}
+let ficus = {sol: "high", water: "regularly", preço: "30", toxicity: "false"}
 
-let bamboo = {nome: "Bamboo", sol: "low", water: "regulary", preço: "15", toxicity: "false"}
+let bamboo = {sol: "low", water: "regularly", preço: "15", toxicity: "false"}
 
-let ponytail = {nome: "Ponytail Palm", sol: "low", water: "regulary", preço: "50", toxicity: "false"}
+let ponytail = {sol: "low", water: "regularly", preço: "50", toxicity: "false"}
 
-let pilea = {nome: "Pilea peperomioides", sol: "no", water: "regulary", preço: "50", toxicity: "true"}
+let pilea = {sol: "no", water: "regularly", preço: "50", toxicity: "true"}
 
-let calathea = {nome: "Calathea triostar", sol: "no", water: "daily", preço: "50", toxicity: "true"}
+let calathea = {sol: "no", water: "daily", preço: "50", toxicity: "true"}
 
-let monstera = {nome: "Monstera deliciosa", sol: "no", water: "daily", preço: "50", toxicity: "true"}
+let monstera = {sol: "no", water: "daily", preço: "50", toxicity: "true"}
 
 let plantas = [euphorbia, succculent, bunny, ficus, bamboo, ponytail, pilea, calathea, monstera]
 
-// html
+// ids em html
 
 let sec_resultados = document.getElementById("resultados")
 let sec_produtos = document.getElementById("produtos")
+let pular = document.getElementById("pular")
 
-function verifica_planta(sun, water, pet) {
+let euphorbia_id = document.getElementById("euphorbia_id")
+let succculent_id = document.getElementById("succulent_id")
+let bunny_id = document.getElementById("bunny_id")
+let ficus_id = document.getElementById("ficus_id")
+let bamboo_id = document.getElementById("bamboo_id")
+let ponytail_id = document.getElementById("ponytail_id")
+let pilea_id = document.getElementById("pilea_id")
+let calathea_id = document.getElementById("calathea_id")
+let monstera_id = document.getElementById("monstera_id")
+
+let plantas_2 = [euphorbia_id, succculent_id, bunny_id, ficus_id, bamboo_id, ponytail_id, pilea_id, calathea_id, monstera_id]
+
+function verifica_sol(sun) {
     sec_resultados.style.display = "none"
     sec_produtos.style.display = "block"
+    pular.style.display = "block"
+
+    for (i = 0; i < 9; i++) {
+        if (plantas_2[i].style.display = "inline-block") {
+            plantas_2[i].style.display = "none"
+        } 
+    }
 
     for (i = 0; i < 9; i++) {
         if (plantas[i]["sol"] == sun) {
-            console.log(plantas[i]["nome"])
-        } else {
-            console.log("Não bate com a pesquisa")
-        }
+            plantas_2[i].style.display = "inline-block"
+        } 
+    }
+}
 
+function verifica_agua(water) {
+    sec_resultados.style.display = "none"
+    sec_produtos.style.display = "block"
+    pular.style.display = "none"
+
+    for (i = 0; i < 9; i++) {
+        if (plantas_2[i].style.display = "inline-block") {
+            plantas_2[i].style.display = "none"
+        } 
+    }
+
+    for (i = 0; i < 9; i++) {
         if (plantas[i]["water"] == water) {
-            console.log(plantas[i]["water"])
-        } else {
-            console.log("Não bate com a pesquisa")
+            plantas_2[i].style.display = "inline-block"
         }
+    }
+}
 
-        if (plantas[i]["pet"] == pet) {
-            console.log(plantas[i]["pet"])
-        } else {
-            console.log("Não bate com a pesquisa")
+function verifica_pets(pets) {
+    sec_resultados.style.display = "none"
+    sec_produtos.style.display = "block"
+    pular.style.display = "block"
+
+    for (i = 0; i < 9; i++) {
+        if (plantas_2[i].style.display = "inline-block") {
+            plantas_2[i].style.display = "none"
+        } 
+    }
+
+    for (i = 0; i < 9; i++) {
+        if (plantas[i]["toxicity"] == pets) {
+            plantas_2[i].style.display = "inline-block"
         }
     }
 }
